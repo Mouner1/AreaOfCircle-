db108e80f758e59903f1dd9d1d2c3a8c3463f733
modab5310 <modab5310@7HJM6Y1.Student.UGDSB.ED>
2018-10-09 09:18:00 -0400

    Java

---
+++ src/area/of/a/circle/AreaOfACircle.java
@@ -0,0 +1,28 @@
+/*
+ * Mouner Dabjan
+ * 18/9/2018
+ * The purpose is to calculoate the area of a circle 
+ */
+
+package area.of.a.circle;
+
+/**
+ *
+ * @author modab5310
+ */
+public class AreaOfACircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double radius = 15.0;
+        double pi = 3.14;
+        double area = 0.0;
+        // algorithim for area  calc
+        area= pi*radius* radius;
+        System.out.println("the area..."+area+"cm sqaured");
+        
+    }
+    
+}
